e87c5c2231220f32e698b3ca34e660128036180f
Manmit Singh <manmits350@gmail.com>
2025-06-06 11:29:21 +0530

    chore: remove swap page

--- src/app/swap/page.tsx
+++
@@ -1,9 +0,0 @@
-import SwapModal from './SwapModal';
-
-export default function Swap() {
-  return (
-    <main className="flex items-center justify-center min-h-screen bg-dark-black-10 text-white p-6">
-      <SwapModal />
-    </main>
-  );
-}
