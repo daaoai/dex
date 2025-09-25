125d812a89c502dbdda26fa51807701c79bb2880
Manraj <65106261+manraj2712@users.noreply.github.com>
2025-09-25 09:21:00 +0530

    chore: add weth address

--- src/constants/navbar.ts
+++ src/constants/navbar.ts
@@ -2,7 +2,7 @@ export const navLinks = [
   // { name: 'Trade', href: '/trade' },
   { name: 'Explore Pools', href: '/explore' },
   { name: 'Positions', href: '/positions' },
-  { name: 'Token Analytics', href: '/token/0xfb5B838b6cfEEdC2873aB27866079AC55363D37E' },
+  { name: 'Token Analytics', href: '/token/0x2170Ed0880ac9A755fd29B2688956BD959F933F8' },
   { name: 'Lending', href: 'https://lending.synthari.io/' },
 
   // { name: 'Trenches', href: '/trenches' },
